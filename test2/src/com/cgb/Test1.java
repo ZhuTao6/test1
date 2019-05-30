030244fc109907f03cf7a1c5e610f908337063c9
tarena <tarena@tarena-pc>
2019-05-30 16:04:16 +0800

    1.4

--- test2/src/com/cgb/Test1.java
+++ test2/src/com/cgb/Test1.java
@@ -4,8 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("111111");
 		System.out.println("222222");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
+		System.out.println("333333");
+		System.out.println("444444");
 	}
 }
