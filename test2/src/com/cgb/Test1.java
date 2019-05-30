5423fa0c94bfa819c512816492f50c429cb4e716
tarena <tarena@tarena-pc>
2019-05-30 16:13:25 +0800

    1.4

--- test2/src/com/cgb/Test1.java
+++ test2/src/com/cgb/Test1.java
@@ -5,6 +5,6 @@ public class Test1 {
 		System.out.println("111111");
 		System.out.println("222222");
 		System.out.println("333333");
-		System.out.println("444444");
+		System.out.println("555555");
 	}
 }
